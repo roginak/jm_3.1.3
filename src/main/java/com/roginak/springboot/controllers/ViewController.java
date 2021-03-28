1088e0b0283553c85c4751db088ddd0a0d372605
IVKan13 <ikanv13@gmail.com>
2021-03-28 20:42:53 +0500

    fix: remove service from viewcontroller

--- src/main/java/com/roginak/springboot/controllers/ViewController.java
+++ src/main/java/com/roginak/springboot/controllers/ViewController.java
@@ -1,6 +1,8 @@
 package com.roginak.springboot.controllers;
 
-import com.roginak.springboot.service.UserService;
+import com.roginak.springboot.entities.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,13 +14,6 @@ import java.security.Principal;
 @RequestMapping("/")
 public class ViewController {
 
-    private final UserService userService;
-
-    public ViewController(UserService userService) {
-        this.userService = userService;
-    }
-
-
     @GetMapping(value = "login")
     public String loginPage() {
         return "login";
@@ -26,16 +21,18 @@ public class ViewController {
 
     @GetMapping("user")
     public String getUserInfo(HttpServletResponse response, Principal p) {
-        int id = userService.getUserByLogin(p.getName()).getId();
-        response.addCookie(new Cookie("currentUserId", Integer.toString(id)));
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
+        response.addCookie(new Cookie("currentUserId", Integer.toString(user.getId())));
         response.addCookie(new Cookie("currentUserLogin", p.getName()));
         return "user/info";
     }
 
     @RequestMapping(value = "admin")
     public String adminPage(HttpServletResponse response, Principal p) {
-        int id = userService.getUserByLogin(p.getName()).getId();
-        response.addCookie(new Cookie("currentUserId", Integer.toString(id)));
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
+        response.addCookie(new Cookie("currentUserId", Integer.toString(user.getId())));
         response.addCookie(new Cookie("currentUserLogin", p.getName()));
         return "admin/main";
     }
